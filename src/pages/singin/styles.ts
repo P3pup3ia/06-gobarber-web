f3b47cbbe8360a34caf24ea098fa244cc866a445
P3pup3ia <robert_sancts@hotmail.com>
2021-01-30 09:12:11 -0300

    refactory

--- src/pages/singin/styles.ts
+++ src/pages/singin/styles.ts
@@ -105,26 +105,3 @@ export const Background = styled.div`
   background: url(${signInBackgroundImg}) no-repeat center;
   background-size: cover;
 `;
-
-// export const Container = styled.div`
-//   height: 100vh;
-
-//   display: flex;
-//   align-items: stretch;
-// `;
-
-// export const Content = styled.div`
-//   display: flex;
-//   flex-direction: column;
-
-//   place-content: center;
-
-//   width: 100%;
-//   max-width: 700%;
-// `;
-
-// export const Background = styled.div`
-//   flex: 1;
-//   background: url(${signInBackgroundImg}) no-repeat center;
-//   background-size: cover;
-// `;
